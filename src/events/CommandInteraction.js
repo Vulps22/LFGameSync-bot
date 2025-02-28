83688fcdc211424ca5751d3bc22604af7a92b442
Vulps <ajmcallister5@gmail.com>
2025-02-28 18:22:39 +0000

    "#18 - Move registration functions to CommandInteraction"

--- src/events/CommandInteraction.js
+++ src/events/CommandInteraction.js
@@ -4,6 +4,8 @@ const Logger = require('../utils/logger.js');
 const commandHandler = require('../utils/commandHandler.js');
 const Command = require('../interfaces/Command.js');
 const { sendTo } = require('../utils/notify.js');
+const DiscordServer = require('../models/discord_server.js');
+const User = require("../models/user.js");
 
 
 // @ts-check
@@ -15,23 +17,27 @@ const interactionEvent = {
 	name: Events.InteractionCreate,
 	once: false,
 	async execute(interaction) {
+
+		await checkServer(interaction);
+		await checkUser(interaction);
+
 		if (interaction.isAutocomplete()) {
 			const command = getCommand(interaction.commandName);
 
-			if (!command) { commandNotFound(interaction.commandName); return;}
+			if (!command) { commandNotFound(interaction.commandName); return; }
 			await command.autoComplete?.(interaction);
 		}
 		if (!interaction.isCommand()) return;
 
 		const command = getCommand(interaction.commandName);
 
-		if (!command) { commandNotFound(interaction.commandName); return;}
+		if (!command) { commandNotFound(interaction.commandName); return; }
 
 		try {
 			await command.execute(interaction);
 		} catch (error) {
 			Logger.log('Interaction', `Failed to execute command ${interaction.commandName}. ${error}`);
-			await interaction.reply({content: `[Interaction] Failed to execute command ${interaction.commandName}.`, ephemeral: true});
+			await interaction.reply({ content: `[Interaction] Failed to execute command ${interaction.commandName}.`, ephemeral: true });
 		}
 	},
 };
@@ -41,7 +47,7 @@ const interactionEvent = {
  * @param {string} commandName 
  * @returns {Command}
  */
-function getCommand(commandName){
+function getCommand(commandName) {
 	const command = commandHandler.getCommands().get(commandName);
 
 	if (!command) {
@@ -53,8 +59,70 @@ function getCommand(commandName){
 }
 
 function commandNotFound(commandName) {
- console.error(`Command Not Found: ${commandName}`);
- sendTo("ERROR", `Command Not Found: ${commandName}`);
+	console.error(`Command Not Found: ${commandName}`);
+	sendTo("ERROR", `Command Not Found: ${commandName}`);
+}
+
+/**
+ * Check if the server exists in the database and ensure the server is up to date.
+ * @param {Interaction} interaction 
+ */
+async function checkServer(interaction) {
+	const guild = interaction.guild;
+	const discordId = guild.id;
+
+	// Check if the server exists
+	const dbServer = await DiscordServer.findOne({
+		where: {
+			discordId: discordId,
+		},
+	});
+
+	if (!dbServer) {
+		DiscordServer.create({
+			discordId: discordId,
+			discordName: guild.name,
+		});
+	} else {
+		// Update the server's name if it has changed
+		if (dbServer.discordName !== guild.name) {
+
+			dbServer.discordName = guild.name;
+			await dbServer.save();
+
+		}
+	}
+
+}
+
+/**
+ * Check if the user exists in the database and ensure the user is up to date.
+ * @param {Interaction} interaction 
+ */
+async function checkUser(interaction) {
+	const user = interaction.user;
+	const discordId = user.id;
+
+	// Check if the user exists
+	const dbUser = await User.findOne({
+		where: {
+			discordId: discordId,
+		},
+	});
+
+	if (!dbUser) {
+		await User.create({
+			discordId: discordId,
+			discordName: user.username,
+		});
+	} else {
+		// Update the user's name if it has changed
+		if (dbUser.discordName !== user.username) {
+			dbUser.discordName = user.username;
+			await dbUser.save();
+		}
+	}
+
 }
 
 module.exports = interactionEvent;
