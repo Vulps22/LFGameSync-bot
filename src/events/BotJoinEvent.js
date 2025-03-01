fc2d826f0999b450b3b970812ce1e3fb8ca106c9
Vulps <ajmcallister5@gmail.com>
2025-03-01 00:50:39 +0000

    #18 - Update botJoinEvent to take advantage of ORM

--- src/events/BotJoinEvent.js
+++ src/events/BotJoinEvent.js
@@ -1,5 +1,7 @@
 const Caller = require('../utils/caller.js');
 const BotEvent = require('../interfaces/botEvent.js');
+const DiscordServer = require('../models/discordServer.js');
+const { Guild } = require('discord.js');
 
 // @ts-check
 
@@ -9,12 +11,19 @@ const BotEvent = require('../interfaces/botEvent.js');
 const BotJoinEvent = {
 	name: "guildCreate",
 	once: false,
+	/**
+	 * 
+	 * @param {Guild} guild 
+	 */
 	async execute(guild) {
 		console.log("Joined a server!")
 
-		// send server ID, name and image to https://gamesync.ajmcallister.co.uk/api/lfg/register_server
 		console.log("Registering Server with data: ");
-		await Caller.registerServer(guild?.id, guild?.name, guild?.iconURL());
+		// Try to find or create the user
+			const [server] = await DiscordServer.findOrCreate({
+				where: { discordId: discordId },
+				defaults: { name: guild.name }
+			});
 	},
 }
 
