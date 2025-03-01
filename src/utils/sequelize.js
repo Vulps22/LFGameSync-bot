8007f7733229b5a3097109ac8188608115fad790
Vulps <ajmcallister5@gmail.com>
2025-03-01 05:33:46 +0000

    #18 remove sequelize config in favour of .env variables

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require('sequelize');
-const config = require('../config'); // Assuming your database config is stored here
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  port: config.port,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   logging: false,
   //logging: console.log,
