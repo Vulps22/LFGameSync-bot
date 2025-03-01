41ece28ce1eb9555bc37cd3090155b1b7aaa1506
Vulps <ajmcallister5@gmail.com>
2025-03-01 19:03:04 +0000

    Add config database

---
+++ src/models/config.js
@@ -0,0 +1,63 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Config extends Model {
+    static associate(models) {
+      // define associations here if needed
+    }
+  }
+  Config.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    clientId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    baseURL: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    linkBaseURL: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    steamApiKey: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    discordLogChannelId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    discordErrorChannelId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    discordServerChannelId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    discordSupportServerId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    env: {
+      type: DataTypes.ENUM('prod', 'dev', 'stage', 'test'),
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'Config',
+    // If you prefer snake_case in the database, uncomment the line below:
+    underscored: true,
+  });
+  return Config;
+};
